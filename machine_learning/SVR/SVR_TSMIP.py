d94a9eb9abfabb39edaf0dd58997c30193a2a8d6
joey <t1616joy@yahoo.com.tw>
2022-09-16 11:56:05 +0800

    use paper method

--- machine_learning/SVR/SVR_TSMIP.py
+++ machine_learning/SVR/SVR_TSMIP.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 df = pd.read_csv("../../../TSMIP_FF.csv")
 # df.head(3)
 
@@ -25,37 +27,57 @@ y = df['PGA']
 
 x_train, x_test, y_train, y_test = train_test_split(x.values, y.values, random_state=10, train_size=0.8)
 
-svr_rbf = GridSearchCV(SVR(kernel='rbf', gamma=0.1),
-                   cv=5,
-                   param_grid={
-                       "C": [1e0, 1e1, 1e2, 1e3],
-                       "gamma": np.logspace(-10, -5, 6)
-                   },
-                   n_jobs=-1)
-# svr_rbf = SVR(C=1e3, kernel='rbf', gamma='auto')
+# follow the paper
+C = max(abs(y_train.mean() + 3 * y_train.std()),abs(y_train.mean() - 3 * y_train.std()))
+
+m = y_train.max()-y_train.min()
+k = 17
+p = m * (0.3 ** (1/k))
+gamma = 1 / (2 * (p**2))
+epsilon = 3* 0.18 * (np.log(29896)/29896)**(1/2)
+
+# GridSearch找最佳參數
+# svr_rbf = GridSearchCV(SVR(kernel='rbf', gamma=0.1),
+#                    cv=6,
+#                    param_grid={
+#                        "C": [1e0, 1e1, 1e2, 1e3],
+#                        "gamma": np.logspace(-10, -5, 6)
+#                    },
+#                    n_jobs=-1)
+
+svr_rbf = SVR(C=C, kernel='rbf', gamma=gamma, epsilon=epsilon)
+
+# 計算成績
+# scores = cross_val_score(svr_rbf,x,y,cv=5,scoring='accuracy')
+# print(scores)
 
 t0 = time.time()
 grid_result = svr_rbf.fit(x_train,y_train)
 fit_time = time.time() - t0
-
 svr_predict = svr_rbf.predict(x_test)
+score = svr_rbf.score(x_test,y_test)
+
+# 評估，打分數
+print("accuracy_score : ",score)
+
+# print(f"最佳準確率: {grid_result.best_score_}，最佳參數組合：{grid_result.best_params_}")
+# # 取得 cross validation 的平均準確率及標準差
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print(f"平均準確率: {mean}, 標準差: {stdev}, 參數組合: {param}")
+
+
+# 依照欄位大小排序，為了畫圖好看
 x_test_tmp_df = pd.DataFrame(x_test)
 x_test_tmp_df.sort_values(by=[1],inplace=True) # inplace 會改變id
 x_test = x_test_tmp_df.to_numpy()
 
-# 評估，打分數
-print(f"最佳準確率: {grid_result.best_score_}，最佳參數組合：{grid_result.best_params_}")
-# 取得 cross validation 的平均準確率及標準差
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print(f"平均準確率: {mean}, 標準差: {stdev}, 參數組合: {param}")
-
 # plt Vs30 and svr_predict relationship
 plt.scatter(x_test[:,0], y_test, marker='o',facecolors='none',edgecolors='b', label= 'Data') #數據點
 plt.scatter(x_test[:,0], svr_predict,marker='o',facecolors='none',edgecolors='r', \
-    label='SVR (fit: %.3fs, accuracy: %.3f)' % (fit_time, grid_result.best_score_)) #迴歸線
+    label='SVR (fit: %.3fs, accuracy: %.3f)' % (fit_time, score)) #迴歸線
 plt.xlabel('lnVs30')
 plt.ylabel('svr_predict')
 plt.title('Support Vector Regression')
@@ -66,7 +88,7 @@ plt.show()
 # plt Mw and svr_predict relationship
 plt.scatter(x_test[:,1], y_test, marker='o',facecolors='none',edgecolors='b', label= 'Data') #數據點
 plt.scatter(x_test[:,1], svr_predict,marker='o',facecolors='none',edgecolors='r', \
-    label='SVR (fit: %.3fs, accuracy: %.3f)' % (fit_time, grid_result.best_score_)) #迴歸線
+    label='SVR (fit: %.3fs, accuracy: %.3f)' % (fit_time, score)) #迴歸線
 plt.xlabel('Mw')
 plt.ylabel('svr_predict')
 plt.title('Support Vector Regression')
@@ -77,7 +99,7 @@ plt.show()
 # plt Rrup and svr_predict relationship
 plt.scatter(x_test[:,2], y_test, marker='o',facecolors='none',edgecolors='b', label= 'Data') #數據點
 plt.scatter(x_test[:,2], svr_predict,marker='o',facecolors='none',edgecolors='r', \
-    label='SVR (fit: %.3fs, accuracy: %.3f)' % (fit_time, grid_result.best_score_)) #迴歸線
+    label='SVR (fit: %.3fs, accuracy: %.3f)' % (fit_time, score)) #迴歸線
 plt.xlabel('lnRrup')
 plt.ylabel('svr_predict')
 plt.title('Support Vector Regression')

3148c004a30b45dca4030b4679cd2c5c1bfb83d0
joey <t1616joy@yahoo.com.tw>
2022-09-18 14:50:14 +0800

    plt taiwan & mw

--- machine_learning/SVR/data_distribution.py
+++ machine_learning/SVR/data_distribution.py
@@ -7,7 +7,7 @@ import pygmt
 df = pd.read_csv("../../../TSMIP_FF.csv")
 df['lnVs30'] = np.log(df['Vs30'])
 df['lnRrup'] = np.log(df['Rrup'])
-
+df['Mw_size'] = np.zeros(len(df['lnRrup']))
 ################### Rrup Mw distribution ###########################
 
 plt.grid(color='gray', linestyle = '--',linewidth=0.5)
@@ -35,18 +35,31 @@ plt.show()
 
 ################### Distribution of the epicenters ###########################
 region = [119.30,124,21.5,26.0]
-Mw_min_df = df[(df['MW'] < 4)]['MW']
-Mw_5_df = df[(df['MW'] >= 4) & (df['MW'] < 5)]['MW']
-Mw_6_df = df[(df['MW'] >= 5) & (df['MW'] < 6)]['MW']
-Mw_max_df = df[(df['MW'] >= 6)]['MW']
+df.loc[(df['MW'] < 4),'Mw_size'] = 0.1
+df.loc[(df['MW'] >= 4) & (df['MW'] < 5),'Mw_size'] = 0.3
+df.loc[(df['MW'] >= 5) & (df['MW'] < 6),'Mw_size'] = 0.5
+df.loc[(df['MW'] >= 6),'Mw_size'] = 0.7
 
 fig = pygmt.Figure()
 # Make a global Mollweide map with automatic ticks
-fig.basemap(region=region, projection="W15c", frame=True)
+fig.basemap(region=region, projection="W15c", frame=["af", f'WSne+t"TSMIP epicneters distribution"'])
 # Plot the land as light gray, and the water as sky blue
 fig.coast(land="#666666", water="skyblue")
 fig.plot(
-    x=df['Hyp.Long'], y=df['Hyp.Lat'], size=df['MW']*0.1,
+    x=df['Hyp.Long'], y=df['Hyp.Lat'], size=df['Mw_size'],
     style="c", color="red", pen="1p,black"
 )
+
+fig.text(x=122.8,y=25.8,text="Mw")
+fig.plot(x=123,y=25.8,color="gray",style="c0.1c",pen="1p,black")
+fig.plot(x=123.2,y=25.8,color="gray",style="c0.3c",pen="1p,black")
+fig.plot(x=123.4,y=25.8,color="gray",style="c0.5c",pen="1p,black")
+fig.plot(x=123.65,y=25.8,color="gray",style="c0.7c",pen="1p,black")
+
+fig.text(x=123,y=25.6,text="<4",font="12p")
+fig.text(x=123.2,y=25.6,text="5",font="12p")
+fig.text(x=123.4,y=25.6,text="6",font="12p")
+fig.text(x=123.65,y=25.6,text=">7",font="12p")
+
+fig.savefig("TSMIP epicneters distribution.png",dpi=300)
 fig.show()

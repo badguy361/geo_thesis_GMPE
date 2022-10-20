64f012171480e8f8fc1f99bb7f9846db1435dd21
joey <t1616joy@yahoo.com.tw>
2022-10-20 18:17:16 +0800

    finish residual figure

--- machine_learning/SVR/SVR_TSMIP_smogn.py
+++ machine_learning/SVR/SVR_TSMIP_smogn.py
@@ -120,7 +120,7 @@ x_train, x_test, y_train, y_test = train_test_split(x.values,
                                                     train_size=0.8,
                                                     shuffle=True)
 
-svr_rbf = SVR(C=1.99, kernel='rbf', epsilon=0.001)
+svr_rbf = SVR(C=1.03, kernel='rbf', epsilon=0.01, gamma=6.62)
 t0 = time.time()
 grid_result = svr_rbf.fit(x_train, y_train)
 fit_time = time.time() - t0
@@ -138,12 +138,63 @@ print("CV後 R2 scores:", scores)
 ######################### residual #########################
 # 1. 計算Vs30_residual
 residual = svr_predict - y_test
+residual_121 = []
+residual_199 = []
+residual_398 = []
+residual_794 = []
+residual_1000 = []
+
+for index, i in enumerate(np.exp(x_test[:, 0])):
+    if i >= 121 and i < 199:
+        residual_121.append(residual[index])
+    elif i >= 199 and i < 398:
+        residual_199.append(residual[index])
+    elif i >= 398 and i < 794:
+        residual_398.append(residual[index])
+    elif i >= 794 and i < 1000:
+        residual_794.append(residual[index])
+    elif i >= 1000:
+        residual_1000.append(residual[index])
+
+residual_121_mean = np.mean(residual_121)
+residual_199_mean = np.mean(residual_199)
+residual_398_mean = np.mean(residual_398)
+residual_794_mean = np.mean(residual_794)
+residual_1000_mean = np.mean(residual_1000)
+
+residual_121_std = np.std(residual_121)
+residual_199_std = np.std(residual_199)
+residual_398_std = np.std(residual_398)
+residual_794_std = np.std(residual_794)
+residual_1000_std = np.std(residual_1000)
 plt.grid(linestyle=':', color='darkgrey')
 plt.scatter(np.exp(x_test[:, 0]),
             residual,
             marker='o',
             facecolors='none',
             edgecolors='r')  #迴歸線
+plt.scatter([121, 199, 398, 794, 1000], [
+    residual_121_mean, residual_199_mean, residual_398_mean, residual_794_mean,
+    residual_1000_mean
+],
+            marker='o',
+            facecolors='none',
+            edgecolors='b')
+plt.plot([121, 121], [
+    residual_121_mean + residual_121_std,residual_121_mean - residual_121_std
+], 'b')
+plt.plot([199, 199], [
+    residual_199_mean + residual_199_std,residual_199_mean - residual_199_std
+], 'b')
+plt.plot([398, 398], [
+    residual_398_mean + residual_398_std,residual_398_mean - residual_398_std
+], 'b')
+plt.plot([794, 794], [
+    residual_794_mean + residual_794_std,residual_794_mean - residual_794_std
+], 'b')
+plt.plot([1000, 1000], [
+    residual_1000_mean + residual_1000_std,residual_1000_mean - residual_1000_std
+], 'b')
 plt.xscale("log")
 plt.xlim(1e2, 2 * 1e3)
 plt.ylim(-3, 3)
@@ -155,12 +206,55 @@ plt.show()
 
 # 2. 計算Mw_residual
 residual = svr_predict - y_test
+residual_3_5 = []
+residual_4_5 = []
+residual_5_5 = []
+residual_6_5 = []
+
+for index, i in enumerate(x_test[:, 1]):
+    if i >= 3.5 and i < 4.5:
+        residual_3_5.append(residual[index])
+    elif i >= 4.5 and i < 5.5:
+        residual_4_5.append(residual[index])
+    elif i >= 5.5 and i < 6.5:
+        residual_5_5.append(residual[index])
+    elif i >= 6.5:
+        residual_6_5.append(residual[index])
+
+residual_3_5_mean = np.mean(residual_3_5)
+residual_4_5_mean = np.mean(residual_4_5)
+residual_5_5_mean = np.mean(residual_5_5)
+residual_6_5_mean = np.mean(residual_6_5)
+
+residual_3_5_std = np.std(residual_3_5)
+residual_4_5_std = np.std(residual_4_5)
+residual_5_5_std = np.std(residual_5_5)
+residual_6_5_std = np.std(residual_6_5)
+
 plt.grid(linestyle=':', color='darkgrey')
 plt.scatter(x_test[:, 1],
             residual,
             marker='o',
             facecolors='none',
             edgecolors='r')  #迴歸線
+plt.scatter([3.5, 4.5, 5.5, 6.5], [
+    residual_3_5_mean, residual_4_5_mean, residual_5_5_mean, residual_6_5_mean
+],
+            marker='o',
+            facecolors='none',
+            edgecolors='b')
+plt.plot([3.5, 3.5], [
+    residual_3_5_mean + residual_3_5_std, residual_3_5_mean - residual_3_5_std
+], 'b')
+plt.plot([4.5, 4.5], [
+    residual_4_5_mean + residual_4_5_std, residual_4_5_mean - residual_4_5_std
+], 'b')
+plt.plot([5.5, 5.5], [
+    residual_5_5_mean + residual_5_5_std, residual_5_5_mean - residual_5_5_std
+], 'b')
+plt.plot([6.5, 6.5], [
+    residual_6_5_mean + residual_6_5_std, residual_6_5_mean - residual_6_5_std
+], 'b')
 plt.xlim(3, 8)
 plt.ylim(-3, 3)
 plt.xlabel('Mw')
@@ -171,12 +265,54 @@ plt.show()
 
 # 3. 計算Rrup_residual
 residual = svr_predict - y_test
+residual_10 = []
+residual_31 = []
+residual_100 = []
+residual_316 = []
+
+for index, i in enumerate(np.exp(x_test[:, 2])):
+    if i >= 10 and i < 31:
+        residual_10.append(residual[index])
+    elif i >= 31 and i < 100:
+        residual_31.append(residual[index])
+    elif i >= 100 and i < 316:
+        residual_100.append(residual[index])
+    elif i >= 316:
+        residual_316.append(residual[index])
+
+residual_10_mean = np.mean(residual_10)
+residual_31_mean = np.mean(residual_31)
+residual_100_mean = np.mean(residual_100)
+residual_316_mean = np.mean(residual_316)
+
+residual_10_std = np.std(residual_10)
+residual_31_std = np.std(residual_31)
+residual_100_std = np.std(residual_100)
+residual_316_std = np.std(residual_316)
 plt.grid(linestyle=':', color='darkgrey')
 plt.scatter(np.exp(x_test[:, 2]),
             residual,
             marker='o',
             facecolors='none',
             edgecolors='r')  #迴歸線
+plt.scatter([10, 31, 100, 316], [
+    residual_10_mean, residual_31_mean, residual_100_mean, residual_316_mean
+],
+            marker='o',
+            facecolors='none',
+            edgecolors='b')
+plt.plot([10, 10], [
+    residual_10_mean + residual_10_std, residual_10_mean - residual_10_std
+], 'b')
+plt.plot([31, 31], [
+    residual_31_mean + residual_31_std, residual_31_mean - residual_31_std
+], 'b')
+plt.plot([100, 100], [
+    residual_100_mean + residual_100_std, residual_100_mean - residual_100_std
+], 'b')
+plt.plot([316, 316], [
+    residual_316_mean + residual_316_std, residual_316_mean - residual_316_std
+], 'b')
 plt.xscale("log")
 plt.xlim(5 * 1e0, 1e3)
 plt.ylim(-3, 3)
@@ -261,8 +397,12 @@ plt.show()
 ###################### 預測PGA和實際PGA #####################
 # training subset
 plt.grid(linestyle=':', color='darkgrey')
-plt.scatter(y_train, svr_predict_train, marker='o', facecolors='none',
-            edgecolors='r',label="Data")  #迴歸線.
+plt.scatter(y_train,
+            svr_predict_train,
+            marker='o',
+            facecolors='none',
+            edgecolors='r',
+            label="Data")  #迴歸線.
 x = [-5, 10]
 y = [-5, 10]
 plt.plot(x, y, color='blue')
@@ -270,8 +410,8 @@ plt.xlabel('Measured ln(PGA)(cm/s^2)')
 plt.ylabel('SVR_Predict ln(PGA)(cm/s^2)')
 plt.ylim(-5, 10)
 plt.xlim(-5, 10)
-plt.text(6,-2,f"R2 score = {round(score_train,2)}")
-plt.text(6,-1,f"MAE = 0.38")
+plt.text(6, -2, f"R2 score = {round(score_train,2)}")
+plt.text(6, -1, f"MAE = 0.38")
 plt.legend()
 plt.title('Measured_Predict Distribution')
 plt.savefig(f'Measured_Predict Training Subset.png', dpi=300)
@@ -279,8 +419,12 @@ plt.show()
 
 # testing subset
 plt.grid(linestyle=':', color='darkgrey')
-plt.scatter(y_test, svr_predict, marker='o', facecolors='none',
-            edgecolors='r',label="Data")  #迴歸線.
+plt.scatter(y_test,
+            svr_predict,
+            marker='o',
+            facecolors='none',
+            edgecolors='r',
+            label="Data")  #迴歸線.
 x = [-5, 10]
 y = [-5, 10]
 plt.plot(x, y, color='blue')
@@ -288,8 +432,8 @@ plt.xlabel('Measured ln(PGA)(cm/s^2)')
 plt.ylabel('SVR_Predict ln(PGA)(cm/s^2)')
 plt.ylim(-5, 10)
 plt.xlim(-5, 10)
-plt.text(6,-2,f"R2 score = {round(score,2)}")
-plt.text(6,-1,f"MAE = 0.40")
+plt.text(6, -2, f"R2 score = {round(score,2)}")
+plt.text(6, -1, f"MAE = 0.40")
 plt.legend()
 plt.title('Measured_Predict Distribution')
 plt.savefig(f'Measured_Predict Testing Subset.png', dpi=300)
--- machine_learning/SVR/optuna_SVR_TSMIP.py
+++ machine_learning/SVR/optuna_SVR_TSMIP.py
@@ -17,25 +17,26 @@ def objective(trial):
     # Invoke suggest methods of a Trial object to generate hyperparameters.
     svr_c = trial.suggest_float('svr_c', 1e-3, 2)
     svr_epsilon = trial.suggest_float('svr_epsilon', 1e-3, 1e-2)
-    regressor_obj = SVR(C=svr_c, epsilon=svr_epsilon)
+    svr_gamma = trial.suggest_float('svr_gamma', 1e-3, 10)
+    regressor_obj = SVR(C=svr_c, epsilon=svr_epsilon, gamma=svr_gamma)
 
-    TSMIP_smogn_df = pd.read_csv("../../../TSMIP_smogn.csv")
+    # TSMIP_smogn_df = pd.read_csv("../../../TSMIP_smogn.csv")
     TSMIP_df = pd.read_csv("../../../TSMIP_FF.csv")
 
-    TSMIP_smogn_df['lnVs30'] = np.log(TSMIP_smogn_df['Vs30'])
-    TSMIP_smogn_df['lnRrup'] = np.log(TSMIP_smogn_df['Rrup'])
-    TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
-        "RO", "1")
-    TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
-        "RV", "1")
-    TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
-        "NM", "2")
-    TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
-        "NO", "2")
-    TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
-        "SS", "3")
-    TSMIP_smogn_df['fault.type'] = pd.to_numeric(TSMIP_smogn_df['fault.type'])
-    TSMIP_smogn_df['lnPGA(gal)'] = np.log(TSMIP_smogn_df['PGA'] * 980)
+    # TSMIP_smogn_df['lnVs30'] = np.log(TSMIP_smogn_df['Vs30'])
+    # TSMIP_smogn_df['lnRrup'] = np.log(TSMIP_smogn_df['Rrup'])
+    # TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
+    #     "RO", "1")
+    # TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
+    #     "RV", "1")
+    # TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
+    #     "NM", "2")
+    # TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
+    #     "NO", "2")
+    # TSMIP_smogn_df['fault.type'] = TSMIP_smogn_df['fault.type'].str.replace(
+    #     "SS", "3")
+    # TSMIP_smogn_df['fault.type'] = pd.to_numeric(TSMIP_smogn_df['fault.type'])
+    # TSMIP_smogn_df['lnPGA(gal)'] = np.log(TSMIP_smogn_df['PGA'] * 980)
 
     TSMIP_df['lnVs30'] = np.log(TSMIP_df['Vs30'])
     TSMIP_df['lnRrup'] = np.log(TSMIP_df['Rrup'])
@@ -50,15 +51,15 @@ def objective(trial):
     x = TSMIP_df.loc[:, ['lnVs30', 'MW', 'lnRrup', 'fault.type']]
     y = TSMIP_df['lnPGA(gal)']
 
-    x_SMOGN = TSMIP_smogn_df.loc[:, ['lnVs30', 'MW', 'lnRrup', 'fault.type']]
-    y_SMOGN = TSMIP_smogn_df['lnPGA(gal)']
+    # x_SMOGN = TSMIP_smogn_df.loc[:, ['lnVs30', 'MW', 'lnRrup', 'fault.type']]
+    # y_SMOGN = TSMIP_smogn_df['lnPGA(gal)']
 
     X_train, X_val, y_train, y_val = train_test_split(x,
                                                       y,
                                                       random_state=0,
                                                       train_size=0.8)
 
-    regressor_obj.fit(x_SMOGN, y_SMOGN)
+    regressor_obj.fit(X_train, y_train)
     y_pred = regressor_obj.predict(X_val)
 
     error = r2_score(y_val, y_pred)
@@ -71,7 +72,9 @@ def objective(trial):
 #SVR_TSMIP_5 SMOGN資料當訓練集 原本資料切20%當測試集
 #SVR_TSMIP_6 從頭來過，把y改成ln(PGA)，找TSMIP的最佳解 -> C=1.99 epsilon=0.001
 #SVR_TSMIP_7 y改成ln(PGA)，SMOGN資料當訓練集 原本資料切20%當測試集，並新增fault.type參數 -> 成效反而比較差
-study_name = 'SVR_TSMIP_6'
+#SVR_TSMIP_8 y改成ln(PGA)，原本資料當訓練集 原本資料切20%當測試集，並新增gamma讓他跑分
+#SVR_TSMIP_9 擴大gamma範圍讓他跑分
+study_name = 'SVR_TSMIP_9'
 study = optuna.create_study(study_name=study_name,
                             storage="mysql://root@localhost/SVR_TSMIP",
                             direction="maximize")  # Create a new study.

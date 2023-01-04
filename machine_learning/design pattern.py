c7ee00239ff361307bf95b6b044aeb0994f5922f
joey <t1616joy@yahoo.com.tw>
2023-01-04 10:47:33 +0800

    GDBT first commit

--- machine_learning/design pattern.py
+++ machine_learning/design pattern.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
@@ -15,7 +16,6 @@ from dtreeviz.trees import dtreeviz
 class dataprocess:
     def preprocess(self,data):
         assert str(type(data)) == "<class 'pandas.core.frame.DataFrame'>" , "please input DataFrame"
-        print(data['Vs30'])
         data['lnVs30'] = np.log(data['Vs30'])
         data['lnRrup'] = np.log(data['Rrup'])
         data['log10Vs30'] = np.log10(data['Vs30'])
@@ -56,22 +56,22 @@ class dataprocess:
             return [x,y]
 
     def training(self,model_name,x_train, x_test, y_train, y_test):
-        assert model_name in ["SVR","RF","XGB"]
+        assert model_name in ["SVR","RF","XGB","GDBT","DNN"]
         if model_name=="RF":
-            
-            randomForestModel = RandomForestRegressor(n_estimators=100,
-                                                    criterion='squared_error',
-                                                    bootstrap=True,
-                                                    oob_score=True,
-                                                    #   verbose=True,
-                                                    n_jobs=-1)
+            rfr_params = {'n_estimators': 100,
+                        'criterion': 'squared_error',
+                        'bootstrap': True,
+                        'oob_score': True,
+                        #   verbose=True,
+                        'n_jobs': -1}
+            randomForestModel = RandomForestRegressor(**rfr_params)
             t0 = time.time()
             grid_result = randomForestModel.fit(x_train, y_train)
             feature_importances = grid_result.feature_importances_
             print("oob_score :", grid_result.oob_score_)
             print("feature importances :", grid_result.feature_importances_)
             fit_time = time.time() - t0
-            randomForest_predict = randomForestModel.predict(x_test)
+            final_predict = randomForestModel.predict(x_test)
             # 評估，打分數
             score = randomForestModel.score(x_test, y_test)
             print("test_R2_score :", score)
@@ -79,7 +79,29 @@ class dataprocess:
             # cross validation
             cv_scores = cross_val_score(randomForestModel, x_train, y_train, cv=6, n_jobs=-1)
             print("cross_val R2 score:", cv_scores)
-        return score,feature_importances,fit_time,randomForest_predict
+        
+        elif model_name=="GDBT":
+            gbr_params = {'n_estimators': 1000,
+                        'max_depth': 3,
+                        'min_samples_split': 5,
+                        'learning_rate': 0.5,
+                        'loss': 'squared_error'}
+            GradientBoostingModel = GradientBoostingRegressor(**gbr_params)
+            t0 = time.time()
+            grid_result = GradientBoostingModel.fit(x_train, y_train)
+            feature_importances = grid_result.feature_importances_
+            print("feature importances :", grid_result.feature_importances_)
+            fit_time = time.time() - t0
+            final_predict = GradientBoostingModel.predict(x_test)
+            # 評估，打分數
+            score = GradientBoostingModel.score(x_test, y_test)
+            print("test_R2_score :", score)
+
+            # cross validation
+            cv_scores = cross_val_score(GradientBoostingModel, x_train, y_train, cv=6, n_jobs=-1)
+            print("cross_val R2 score:", cv_scores)
+
+        return score,feature_importances,fit_time,final_predict
     
 
 if __name__=='__main__':
@@ -87,5 +109,5 @@ if __name__=='__main__':
     TSMIP_df = pd.read_csv("../../TSMIP_FF_copy.csv")
     model = dataprocess()
     after_process_data = model.preprocess(TSMIP_smogn_df)
-    a = model.split_dataset(TSMIP_smogn_df,'lnPGA(gal)',True,'lnVs30', 'MW', 'lnRrup', 'fault.type','STA_Lon_X','STA_Lat_Y')
-    b = model.training("RF",a[0],a[1],a[2],a[3])
+    result_list = model.split_dataset(TSMIP_smogn_df,'lnPGA(gal)',True,'lnVs30', 'MW', 'lnRrup', 'fault.type','STA_Lon_X','STA_Lat_Y')
+    score,feature_importances,fit_time,final_predict = model.training("GDBT",result_list[0],result_list[1],result_list[2],result_list[3])

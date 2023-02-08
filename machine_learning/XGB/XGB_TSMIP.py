39933394f3923c5ab05a09ba347f9ca32cee91ca
joey <t1616joy@yahoo.com.tw>
2023-02-08 17:32:14 +0800

    push EQ_ID dataset

--- machine_learning/XGB/XGB_TSMIP.py
+++ machine_learning/XGB/XGB_TSMIP.py
@@ -18,14 +18,17 @@ TSMIP_df = pd.read_csv(f"../../../TSMIP_FF_{target}.csv")
 model = dataprocess()
 after_process_SMOGN_data = model.preprocess(TSMIP_smogn_df, target)
 after_process_ori_data = model.preprocess(TSMIP_df, target)
-result_SMOGN = model.split_dataset(TSMIP_smogn_df, f'ln{target}(gal)', True,
+result_SMOGN = model.split_dataset(after_process_SMOGN_data, f'ln{target}(gal)', True,
                                    'lnVs30', 'MW', 'lnRrup', 'fault.type',
                                    'STA_Lon_X', 'STA_Lat_Y')
-result_ori = model.split_dataset(TSMIP_df, f'ln{target}(gal)', True, 'lnVs30',
+result_ori = model.split_dataset(after_process_ori_data, f'ln{target}(gal)', True, 'lnVs30',
                                  'MW', 'lnRrup', 'fault.type', 'STA_Lon_X',
                                  'STA_Lat_Y')
-original_data = model.split_dataset(TSMIP_df, f'ln{target}(gal)', False,
+original_data = model.split_dataset(after_process_ori_data, f'ln{target}(gal)', False,
                                     'lnVs30', 'MW', 'lnRrup', 'fault.type',
+                                    'STA_Lon_X', 'STA_Lat_Y')
+original_EQID_data = model.split_dataset(after_process_ori_data, f'ln{target}(gal)', False,
+                                    'EQ_ID', 'lnVs30', 'MW', 'lnRrup', 'fault.type',
                                     'STA_Lon_X', 'STA_Lat_Y')
 # result_ori[0](訓練資料)之shape : (29896,6) 為 29896筆 records 加上以下6個columns ['lnVs30','MW', 'lnRrup', 'fault.type', 'STA_Lon_X', 'STA_Lat_Y']
 score, feature_importances, fit_time, final_predict, ML_model = model.training(

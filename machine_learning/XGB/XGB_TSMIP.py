f183e95a6bf498857eb3d4b6dc1e195e8ef522ab
joey <t1616joy@yahoo.com.tw>
2023-02-10 11:55:49 +0800

    inter inta event

--- machine_learning/XGB/XGB_TSMIP.py
+++ machine_learning/XGB/XGB_TSMIP.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
 import matplotlib.pyplot as plt
 
-target = "PGV"
+target = "PGA"
 
 TSMIP_smogn_df = pd.read_csv(f"../../../TSMIP_smogn_{target}.csv")
 TSMIP_df = pd.read_csv(f"../../../TSMIP_FF_{target}.csv")
@@ -28,11 +28,6 @@ result_ori = model.split_dataset(after_process_ori_data, f'ln{target}(gal)',
 original_data = model.split_dataset(after_process_ori_data, f'ln{target}(gal)',
                                     False, 'lnVs30', 'MW', 'lnRrup',
                                     'fault.type', 'STA_Lon_X', 'STA_Lat_Y')
-original_EQID_data = model.split_dataset(after_process_ori_data,
-                                         f'ln{target}(gal)', False, 'EQ_ID',
-                                         'lnVs30', 'MW', 'lnRrup',
-                                         'fault.type', 'STA_Lon_X',
-                                         'STA_Lat_Y')
 # result_ori[0](訓練資料)之shape : (29896,6) 為 29896筆 records 加上以下6個columns ['lnVs30','MW', 'lnRrup', 'fault.type', 'STA_Lon_X', 'STA_Lat_Y']
 score, feature_importances, fit_time, final_predict, ML_model = model.training(
     "XGB", result_SMOGN[0], result_ori[1], result_SMOGN[2], result_ori[3])
@@ -42,29 +37,46 @@ originaldata_predicted_result = model.predicted_original(
 
 residual = final_predict - result_ori[3]
 
-plot_something = plot_fig("XGBooster", "XGB", "SMOGN", target)
+# plot_something = plot_fig("XGBooster", "XGB", "SMOGN", target)
 # plot_something.train_test_distribution(result_ori[1], result_ori[3],
 #                                        final_predict, fit_time, score)
-plot_something.residual(result_ori[1], result_ori[3], final_predict, score) 
-plot_something.measured_predict(result_ori[3], final_predict, score) 
+# plot_something.residual(result_ori[1], result_ori[3], final_predict, score) 
+# plot_something.measured_predict(result_ori[3], final_predict, score) # 應該要改成全部的資料
 # minVs30 = 480
 # maxVs30 = 760
 # minMw = 4.0
 # maxMw = 5.0
 # faulttype = 1
-# plot_something.distance_scaling(original_data, originaldata_predicted_result, # 應該要改成只有test的資料
+# plot_something.distance_scaling(original_data, originaldata_predicted_result, 
 #                                 minVs30, maxVs30, minMw, maxMw, faulttype,
 #                                 score,5)
 
-final_predict_df = pd.DataFrame(final_predict, columns=['predicted'])
-ans_predict_df = pd.DataFrame(result_ori[3], columns=['ans'])
-features_predict_df = pd.DataFrame(
-    result_ori[1],
-    columns=['lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_Lon_X', 'STA_Lat_Y'])
-features_predict_df = pd.DataFrame(
-    original_EQID_data[1],
-    columns=['lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_Lon_X', 'STA_Lat_Y'])
+originaldata_predicted_result_df = pd.DataFrame(originaldata_predicted_result, columns=['predicted'])
+total_data_df = pd.concat([after_process_ori_data,originaldata_predicted_result_df],axis=1)
+total_data_df["residual"] = np.abs((np.exp(total_data_df["predicted"]) - np.exp(total_data_df["lnPGA(gal)"]))/980)
 
 # 重複數據 273顆地震
-# summeries = {'PGV': 'mean'}
-# TSMIP_df.groupby(by="EQ_ID").agg(summeries).reset_index()
+summeries = {'residual': 'mean','MW': 'max'}
+inter_event = total_data_df.groupby(by="EQ_ID").agg(summeries).reset_index()
+inter_event = inter_event.rename(columns = {'residual':'inter_event_residual','MW':'Mw'})
+
+plt.grid(linestyle=':', color='darkgrey')
+plt.scatter(inter_event['Mw'],
+            inter_event['inter_event_residual'],
+            marker='o',
+            facecolors='none',
+            edgecolors='r')  #迴歸線
+plt.show()
+
+total_data_df = pd.merge(total_data_df, inter_event, how='left', on=['EQ_ID'])
+total_data_df['intra_event_residual'] = total_data_df['residual'] - total_data_df['inter_event_residual']
+
+xticks = [0, 50, 100, 150, 200, 250, 300, 400, 500]
+plt.grid(linestyle=':', color='darkgrey')
+plt.scatter(total_data_df['Rrup'],
+            total_data_df['intra_event_residual'],
+            marker='o',
+            facecolors='none',
+            edgecolors='r')  #迴歸線
+plt.xticks(xticks)
+plt.show()

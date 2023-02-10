f4f5ecb8325e0cd21f86117e2687a2fe2e5aea01
joey <t1616joy@yahoo.com.tw>
2023-02-10 16:17:12 +0800

    plot total residual figure and refresh png to jpg

--- machine_learning/RF/RFR_TSMIP.py
+++ machine_learning/RF/RFR_TSMIP.py
@@ -14,15 +14,16 @@ TSMIP_df = pd.read_csv(f"../../../TSMIP_FF_{target}.csv")
 model = dataprocess()
 after_process_SMOGN_data = model.preprocess(TSMIP_smogn_df, target)
 after_process_ori_data = model.preprocess(TSMIP_df, target)
-result_SMOGN = model.split_dataset(TSMIP_smogn_df, f'ln{target}(gal)', True,
-                                   'lnVs30', 'MW', 'lnRrup', 'fault.type',
-                                   'STA_Lon_X', 'STA_Lat_Y')
-result_ori = model.split_dataset(TSMIP_df, f'ln{target}(gal)', True, 'lnVs30',
-                                 'MW', 'lnRrup', 'fault.type', 'STA_Lon_X',
-                                 'STA_Lat_Y')
-original_data = model.split_dataset(TSMIP_df, f'ln{target}(gal)', False,
-                                    'lnVs30', 'MW', 'lnRrup', 'fault.type',
-                                    'STA_Lon_X', 'STA_Lat_Y')
+result_SMOGN = model.split_dataset(after_process_SMOGN_data,
+                                   f'ln{target}(gal)', True, 'lnVs30', 'MW',
+                                   'lnRrup', 'fault.type', 'STA_Lon_X',
+                                   'STA_Lat_Y')
+result_ori = model.split_dataset(after_process_ori_data, f'ln{target}(gal)',
+                                 True, 'lnVs30', 'MW', 'lnRrup', 'fault.type',
+                                 'STA_Lon_X', 'STA_Lat_Y')
+original_data = model.split_dataset(after_process_ori_data, f'ln{target}(gal)',
+                                    False, 'lnVs30', 'MW', 'lnRrup',
+                                    'fault.type', 'STA_Lon_X', 'STA_Lat_Y')
 score, feature_importances, fit_time, final_predict, ML_model = model.training(
     "RF", result_SMOGN[0], result_ori[1], result_SMOGN[2], result_ori[3])
 
@@ -30,15 +31,19 @@ originaldata_predicted_result = model.predicted_original(
     ML_model, original_data)
 
 plot_something = plot_fig("Random Forest Regression", "RF", "SMOGN", target)
-# plot_something.train_test_distribution(result_ori[1], result_ori[3],
-#                                        final_predict, fit_time, score)
-# plot_something.residual(result_ori[1], result_ori[3], final_predict, score)
-# plot_something.measured_predict(result_ori[3], final_predict, score)
-minVs30 = 480
-maxVs30 = 760
-minMw = 7.0
-maxMw = 8.0
-faulttype = 1
-plot_something.distance_scaling(original_data, originaldata_predicted_result,
-                                minVs30, maxVs30, minMw, maxMw, faulttype,
-                                score)
+plot_something.predicted_distribution(result_ori[1], result_ori[3],
+                                      final_predict, fit_time, score)
+plot_something.residual(original_data[0], original_data[1],
+                        originaldata_predicted_result, after_process_ori_data,
+                        score)
+plot_something.measured_predict(original_data[1],
+                                originaldata_predicted_result, score)
+
+# minVs30 = 480
+# maxVs30 = 760
+# minMw = 7.0
+# maxMw = 8.0
+# faulttype = 1
+# plot_something.distance_scaling(original_data, originaldata_predicted_result,
+#                                 minVs30, maxVs30, minMw, maxMw, faulttype,
+#                                 score)
--- machine_learning/XGB/XGB_TSMIP.py
+++ machine_learning/XGB/XGB_TSMIP.py
@@ -35,18 +35,19 @@ score, feature_importances, fit_time, final_predict, ML_model = model.training(
 originaldata_predicted_result = model.predicted_original(
     ML_model, original_data)
 
-
 plot_something = plot_fig("XGBooster", "XGB", "SMOGN", target)
-plot_something.predicted_distribution(result_ori[1], result_ori[3],
-                                       final_predict, fit_time, score)
-plot_something.residual(original_data[0], original_data[1], originaldata_predicted_result, after_process_ori_data, score)
+# plot_something.predicted_distribution(result_ori[1], result_ori[3],
+#                                        final_predict, fit_time, score)
+plot_something.residual(original_data[0], original_data[1],
+                        originaldata_predicted_result, after_process_ori_data,
+                        score)
 # plot_something.measured_predict(original_data[1], originaldata_predicted_result, score)
+
 # minVs30 = 480
 # maxVs30 = 760
 # minMw = 4.0
 # maxMw = 5.0
 # faulttype = 1
-# plot_something.distance_scaling(original_data, originaldata_predicted_result, 
+# plot_something.distance_scaling(original_data, originaldata_predicted_result,
 #                                 minVs30, maxVs30, minMw, maxMw, faulttype,
 #                                 score,5)
-

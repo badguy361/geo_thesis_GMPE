9793f94fb3e8c7b359b664cd884dfc6a69d0e5b9
joey <t1616joy@yahoo.com.tw>
2023-05-08 22:55:31 +0800

    add train predicted

--- machine_learning/design_pattern/tests/test_process_train.py
+++ machine_learning/design_pattern/tests/test_process_train.py
@@ -44,9 +44,7 @@ class Testprocess_train(unittest.TestCase):
         data = pd.read_csv("/TSMIP/TSMIP_FF_test.csv")
         after_process_data = model.preprocess(data, target, True)
 
-        model_feture = [
-            'lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank'
-        ]
+        model_feture = ['lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank']
         result_ori = model.split_dataset(after_process_data,
                                          f'ln{target}(gal)', True,
                                          *model_feture)
@@ -58,59 +56,55 @@ class Testprocess_train(unittest.TestCase):
                                                             random_state=50,
                                                             train_size=0.8,
                                                             shuffle=True)
-        self.assertEqual(round(x_train[0][0],2), round(result_ori[0][0][0],2)) 
+        self.assertEqual(round(x_train[0][0], 2),
+                         round(result_ori[0][0][0], 2))
 
-    # def test_training(self):
-    #     target = "PGA"
-    #     data = pd.read_csv("/TSMIP/TSMIP_FF_test.csv")
-    #     model = dataprocess()
-    #     after_process_data = model.preprocess(data, target, True)
-    #     model_feture = [
-    #         'lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank'
-    #     ]
-    #     result_ori = model.split_dataset(after_process_data,
-    #                                      f'ln{target}(gal)', True,
-    #                                      *model_feture)
-    #     score_model, feature_importances_model, fit_time, final_predict_model, ML_model = model.training(
-    #         target, "XGB", result_ori[0], result_ori[1], result_ori[2],
-    #         result_ori[3])
+    def test_training(self):
+        target = "PGA"
+        data = pd.read_csv("/TSMIP/TSMIP_FF_test.csv")
+        model = dataprocess()
+        after_process_data = model.preprocess(data, target, True)
+        model_feture = ['lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank']
+        result_ori = model.split_dataset(after_process_data,
+                                         f'ln{target}(gal)', True,
+                                         *model_feture)
+        score_model, feature_importances_model, fit_time, final_predict_model, ML_model = model.training(
+            target, "XGB", result_ori[0], result_ori[1], result_ori[2],
+            result_ori[3])
 
-    #     XGB_params = {'n_estimators': 1000, 'max_depth': 10, 'n_jobs': -1}
-    #     XGBModel = XGBRegressor(**XGB_params)
-    #     t0 = time.time()
-    #     grid_result = XGBModel.fit(result_ori[0], result_ori[2])
-    #     feature_importances_test = grid_result.feature_importances_
-    #     fit_time = time.time() - t0
-    #     final_predict_test = XGBModel.predict(result_ori[1])
-    #     score_test = XGBModel.score(result_ori[1], result_ori[3])
-    #     model = XGBModel
+        XGB_params = {'n_estimators': 1000, 'max_depth': 10, 'n_jobs': -1}
+        XGBModel = XGBRegressor(**XGB_params)
+        t0 = time.time()
+        grid_result = XGBModel.fit(result_ori[0], result_ori[2])
+        feature_importances_test = grid_result.feature_importances_
+        fit_time = time.time() - t0
+        final_predict_test = XGBModel.predict(result_ori[1])
+        score_test = XGBModel.score(result_ori[1], result_ori[3])
+        model = XGBModel
 
-    #     assert self.assertEqual(score_model, score_test)
+        self.assertEqual(score_model, score_test)
 
-    # def test_predicted_original(self):
-    #     target = "PGA"
-    #     data = pd.read_csv("/TSMIP/TSMIP_FF_test.csv")
-    #     model_feture = [
-    #         'lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank'
-    #     ]
-    #     after_process_ori_data = model.preprocess(data, target, True)
-    #     original_data = model.split_dataset(after_process_ori_data,
-    #                                         f'ln{target}(gal)', False,
-    #                                         *model_feture)
-    #     ML_model = pickle.load(
-    #         open(f'/TSMIP/machine_learning/XGB/model/XGB_{target}.pkl', 'rb'))
-    #     originaldata_predicted_result = model.predicted_original(
-    #         ML_model, original_data)
+    def test_predicted_original(self):
+        target = "PGA"
+        data = pd.read_csv("/TSMIP/TSMIP_FF_test.csv")
+        model_feture = ['lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank']
+        after_process_ori_data = model.preprocess(data, target, True)
+        original_data = model.split_dataset(after_process_ori_data,
+                                            f'ln{target}(gal)', False,
+                                            *model_feture)
+        ML_model = pickle.load(
+            open(f'/TSMIP/machine_learning/XGB/model/XGB_{target}.pkl', 'rb'))
+        originaldata_predicted_result = model.predicted_original(
+            ML_model, original_data)
 
-    #     predicted_result = ML_model.predict(original_data[0])
-    #     assert self.assertEqual(originaldata_predicted_result[0],
-    #                               predicted_result[0])
+        predicted_result = ML_model.predict(original_data[0])
+        self.assertEqual(originaldata_predicted_result[0], predicted_result[0])
 
 
 suite = unittest.TestSuite()
 suite.addTest(Testprocess_train('test_preprocess'))
 suite.addTest(Testprocess_train('test_split_dataset'))
-# suite.addTest(Testprocess_train('test_training'))
-# suite.addTest(Testprocess_train('test_predicted_original'))
+suite.addTest(Testprocess_train('test_training'))
+suite.addTest(Testprocess_train('test_predicted_original'))
 
 unittest.TextTestRunner(verbosity=2).run(suite)

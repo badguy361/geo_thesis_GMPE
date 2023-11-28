89365bdd86083af816d2d4d1161f8158cafe6bf5
joey <t1616joy@yahoo.com.tw>
2023-11-28 16:44:08 +0800

    store score

--- machine_learning/XGB/XGB_TSMIP.py
+++ machine_learning/XGB/XGB_TSMIP.py
@@ -8,11 +8,11 @@ from modules.process_train import dataprocess
 from modules.plot_figure import plot_fig
 
 #? parameters
-target = "PGV"
+target = "Sa100"
 Mw = 5
 Rrup = 30
 Vs30 = 360
-fault_type = 90
+rake = 90
 station_rank = 265
 model_name = [
     'model/XGB_PGA.json', 'model/XGB_PGV.json', 'model/XGB_Sa001.json',
@@ -21,7 +21,11 @@ model_name = [
     'model/XGB_Sa30.json', 'model/XGB_Sa40.json', 'model/XGB_Sa100.json'
 ]
 seed = 18989
-score = 0.88 # TODO
+score = {
+    'XGB_PGA': 0.88, 'XGB_PGV': 0.89, 'XGB_Sa001': 0.87, 'XGB_Sa005': 0.88,
+    'XGB_Sa01': 0.87 , 'XGB_Sa02': 0.87, 'XGB_Sa03': 0.85, 'XGB_Sa05': 0.86,
+    'XGB_Sa10': 0.90, 'XGB_Sa30': 0.93, 'XGB_Sa40': 0.94, 'XGB_Sa100': 0.93
+}
 lowerbound = 2
 higherbound = 12
 
@@ -42,18 +46,18 @@ original_data = model.splitDataset(after_process_ori_data, f'ln{target}(gal)',
 
 #? model train
 #! result_ori[0](訓練資料)之shape : (29896,5) 為 29896筆 records 加上以下5個columns ['lnVs30', 'MW', 'lnRrup', 'fault.type', 'STA_rank']
-# score, feature_importances, fit_time, final_predict, ML_model = model.training(
-#     target, "XGB", result_SMOGN[0], result_ori[1], result_SMOGN[2], result_ori[3])
+score, feature_importances, fit_time, final_predict, ML_model = model.training(
+    target, "XGB", result_SMOGN[0], result_ori[1], result_SMOGN[2], result_ori[3])
 
 #? model predicted
-booster = xgb.Booster()
-booster.load_model(f'model/XGB_{target}.json')
+# booster = xgb.Booster()
+# booster.load_model(f'model/XGB_{target}.json')
 # booster.predict(xgb.DMatrix([(np.log(760), 7, np.log(200), -45, 256)]))
 # originaldata_predicted_result = model.predicted_original(
 #     booster, original_data)
 
 #? plot figure
-plot_something = plot_fig("XGBooster", "XGB", "SMOGN", target)
+# plot_something = plot_fig("XGBooster", "XGB", "SMOGN", target)
 # plot_something.predicted_distri|bution(result_ori[1], result_ori[3],
 #                                        final_predict, fit_time, score)
 # plot_something.residual(original_data[0], original_data[1],
@@ -62,6 +66,6 @@ plot_something = plot_fig("XGBooster", "XGB", "SMOGN", target)
 # plot_something.measured_predict(original_data[1], originaldata_predicted_result, score, lowerbound, higherbound)
 # plot_something.distance_scaling(Vs30, Mw, Rrup, fault_type, station_rank,
 #                                 original_data[0], original_data[1], booster) # 可取代
-plot_something.respond_spetrum(Vs30, Mw, Rrup, fault_type, station_rank,
-                               False, *model_name)
+# plot_something.respond_spetrum(Vs30, Mw, Rrup, rake, station_rank,
+#                                False, *model_name)
 # plot_something.explainable(original_data[0], model_feture, booster, seed)
